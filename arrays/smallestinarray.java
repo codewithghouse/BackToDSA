daf789079aaf5c1d2f18e802dd606dbcd855de04
codewithghouse <personalghouse@gmail.com>
2026-05-27 01:20:30 +0530

    smallest in an array

---
+++ arrays/smallestinarray.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class smallestinarray {
+    //function to prin the smallest number in an array
+    public static void smallestInArray(){
+        int array[]={3,7,2,9,1};
+        int smallNum=Integer.MAX_VALUE;
+        for(int i=0;i<array.length;i++){
+            if(smallNum>array[i]){
+                smallNum=array[i];
+            }
+        }
+        System.out.print(smallNum);
+    }
+    public static void main(String[] args) {
+        //function call
+        smallestInArray();
+        
+    }
+}
