2a40dac9abd7562fd80133843a5b71c137c3bbaa
codewithghouse <personalghouse@gmail.com>
2026-06-30 06:57:59 +0530

    revies largest in array

---
+++ array/largestinarray.java
@@ -0,0 +1,19 @@
+package array;
+
+public class largestinarray {
+    public static int largestinArray(int array[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0;i<array.length;i++){
+            if(largest < array[i]){
+                largest =array[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int array[]={1,5,8,9,10};
+        //function call
+        System.out.println("the largest element is "+" "+largestinArray(array));
+
+    }
+}
